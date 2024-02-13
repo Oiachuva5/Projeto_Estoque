7547168e6ffe441bce3fb9cfa416e13932ce6abe
oiachuva5 <oiachuva5@gmail.com>
2024-02-12 21:17:17 -0300

    Finalizado crud de customer

--- src/main/kotlin/com/estoquemga/controller/CustomerController.kt
+++ src/main/kotlin/com/estoquemga/controller/CustomerController.kt
@@ -1,10 +1,53 @@
 package com.estoquemga.controller
 
+import com.estoquemga.controller.request.customerRequest.PostCustomerRequest
+import com.estoquemga.controller.request.customerRequest.PutCustomerRequest
+import com.estoquemga.extension.toCustomerModel
+import com.estoquemga.model.CustomerModel
+import com.estoquemga.service.CustomerService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("customers")
-class CustomerController {
+class CustomerController(private val customerService: CustomerService) {
 
+
+    @GetMapping
+    fun getAll(): List<CustomerModel>{
+        return customerService.getAll()
+    }
+
+
+    @PostMapping("/cadastrar")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest){
+        customerService.create(customer.toCustomerModel())
+    }
+
+    @PutMapping("/{id}")
+    fun update(@PathVariable id: Int, @RequestBody customer: PutCustomerRequest){
+        //verificar se é possivel utilizar sem mapear
+//        val updateCustomer = PutCustomerRequest(
+//                id = id,
+//                nomeCompleto = customer.nomeCompleto,
+//                email = customer.email,
+//                senha = customer.senha
+//        )
+        customerService.update(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int){
+        customerService.delete(id)
+    }
 }
--- src/main/kotlin/com/estoquemga/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/estoquemga/extension/ConverterExtensionFunction.kt
@@ -1,5 +1,7 @@
 package com.estoquemga.extension
 
+import com.estoquemga.controller.request.customerRequest.PostCustomerRequest
+import com.estoquemga.controller.request.customerRequest.PutCustomerRequest
 import com.estoquemga.model.NotebookModel
 import com.estoquemga.model.PerifericoModel
 import com.estoquemga.model.SaidaPerifericoModel
@@ -8,45 +10,70 @@ import com.estoquemga.controller.request.perifericoRequest.PostPerifericoRequest
 import com.estoquemga.controller.request.notebookRequest.PutNotebookRequest
 import com.estoquemga.controller.request.perifericoRequest.PutPerifericoRequest
 import com.estoquemga.controller.request.saidaPerifericoRequest.PostSaidaPerifericoRequest
+import com.estoquemga.enums.CustomerStatus
+import com.estoquemga.model.CustomerModel
+
+fun PostCustomerRequest.toCustomerModel(): CustomerModel {
+    return CustomerModel(
+            nomeCompleto = this.nomeCompleto,
+            login = this.login,
+            email = this.email,
+            senha = this.senha,
+            status = CustomerStatus.ATIVO
+    )
+}
+
+fun PutCustomerRequest.toCustomerModel(previousValue: CustomerModel): CustomerModel {
+    return CustomerModel(
+            nomeCompleto = this.nomeCompleto,
+            login = previousValue.login,
+            email = this.email,
+            senha = this.senha,
+            status = previousValue.status
+
+    )
+}
 
 fun PostNotebookRequest.toNotebookModel(): NotebookModel {
     return NotebookModel(
-        hostname = this.hostname,
-        patrimonio = this.patrimonio,
-        serialNumber = this.serialNumber,
-        modelo = this.modelo,
-        situacao = this.situacao,
-        numeroChamado = this.numeroChamado
+            hostname = this.hostname,
+            patrimonio = this.patrimonio,
+            serialNumber = this.serialNumber,
+            modelo = this.modelo,
+            situacao = this.situacao,
+            numeroChamado = this.numeroChamado
     )
 }
 
 fun PutNotebookRequest.toNotebookModel(hostname: String): NotebookModel {
     return NotebookModel(
-        hostname = this.hostname,
-        patrimonio = this.patrimonio,
-        serialNumber = this.serialNumber,
-        modelo = this.modelo,
-        situacao = this.situacao,
-        numeroChamado = this.numeroChamado
+            hostname = this.hostname,
+            patrimonio = this.patrimonio,
+            serialNumber = this.serialNumber,
+            modelo = this.modelo,
+            situacao = this.situacao,
+            numeroChamado = this.numeroChamado
     )
 }
 
 fun PostPerifericoRequest.toPerifericoModel(): PerifericoModel {
     return PerifericoModel(
-        nomeItem = this.nomeItem, quantidade = this.quantidade
+            nomeItem = this.nomeItem,
+            quantidade = this.quantidade
     )
 }
 
 fun PutPerifericoRequest.toPerifericoModel(): PerifericoModel {
     return PerifericoModel(
-        nomeItem = this.nomeItem, quantidade = this.quantidade
+            nomeItem = this.nomeItem,
+            quantidade = this.quantidade
     )
 }
 
 fun PostSaidaPerifericoRequest.toSaidaPerifericoModel(): SaidaPerifericoModel {
     return SaidaPerifericoModel(
-        nomeItem = this.nomeItem,
-        numeroChamado = this.numeroChamado
+            nomeItem = this.nomeItem,
+            numeroChamado = this.numeroChamado
     )
 
 }
